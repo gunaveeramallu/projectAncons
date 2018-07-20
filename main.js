40e07cd6cef7b59f54497c91afab50339cb12b55
gunaveeramallu <39434393+gunaveeramallu@users.noreply.github.com>
2018-07-20 09:53:53 +0530

    new1 changes

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ var db = firebase.firestore();
 
  function sendFeedback(){
     var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var msg = document.getElementById("msg").value;    
+    var cell= document.getElementById("cell").value;  
+    var gmail1 = document.getElementById("gmail1").value;
+    var msg = document.getElementById("msg").value;
+     
 
-    console.log(name,email,msg);
+    console.log(name,cell,gmail1,msg,);
     db.collection('feedback').add({
         name: name,
-        email: email,
+        cell:cell,
+        gmail1: gmail1,
         msg: msg
+        
     })
     .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
@@ -19,7 +23,9 @@ var db = firebase.firestore();
         console.error("Error adding document: ", error);
     });
      document.getElementById("name").value ="";
-     document.getElementById("email").value="";
+     document.getElementById("cell").value="";
+     document.getElementById("gmail1").value="";
      document.getElementById("msg").value="";
+     
 
   }
